d0936de20126e9a5c0b8a0b2b984db4c1c50d84b
Ismaelblog <ismaelblog@hotmail.com>
2026-04-01 19:54:30 -0300

    Add section about, past downloads and updates.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,10 +4,26 @@ const rootHtml = document.documentElement;
 function changeTheme() {
   const currentTheme = rootHtml.getAttribute("data-theme");
 
- currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+  currentTheme === "dark"
+    ? rootHtml.setAttribute("data-theme", "light")
+    : rootHtml.setAttribute("data-theme", "dark");
 
   toggleTheme.classList.toggle("bi-sun");
   toggleTheme.classList.toggle("bi-moon");
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+// Fechamento e abertura do acordeão dos cursos
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach((header) => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive
+      ? accordionItem.classList.remove("active")
+      : accordionItem.classList.add("active");
+  });
+});
